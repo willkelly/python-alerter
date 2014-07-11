99bf11b2d3fe4075fd5368a8aa02e9f9e81a3207
rpedde <ron@pedde.com>
2014-07-11 17:58:06 -0500

    stupid typos

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup
-from alerter import __version__ as version
+
+version = '0.0.1'
 
 setup(
     name = 'alerter',
     version = version,
     author = 'Ron Pedde',
-    author_email = 'ron@pedde.com'
+    author_email = 'ron@pedde.com',
     description = 'pagerduty/collectd framework for python scripts',
     packages = ['alerter'],
     classifiers = ['Development Status :: 4 - Beta',
                    'License :: OSI Approved :: Apache Software License',
                    'Operating System :: POSIX :: Linux',
                    'Programming Language :: Python :: 2.7'],
-    install_requires = [pygerduty])
+    install_requires = ['pygerduty'])
